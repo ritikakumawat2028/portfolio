2837954ea8c56a5a8f49f2d0638268665baf1fb0
ritikakumawat2028 <ritikakumawat2028@gmail.com>
2025-12-21 09:43:23 +0530

    fix all

--- main.js
+++ main.js
@@ -1,7 +1,43 @@
 var typed= new Typed(".text",{
-    strings:["Frontend Deveploper","Web Devloper"],
+    strings:["Frontend Developer","Web Developer"],
     typedSpeed:100,
     backSpeed:100,
     backDelay:1000,
     loop:true
 });
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                let activeLink = document.querySelector('header nav a[href*=' + id + ']');
+                if (activeLink) {
+                    activeLink.classList.add('active');
+                }
+            });
+        };
+    });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
